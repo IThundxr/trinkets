53a0aa3b4ab032f1548e82e026243d007a332694
IThundxr <me@ithundxr.dev>
2024-06-27 12:56:05 -0400

    fix packet registration on fabric

--- fabric/src/main/java/dev/emi/trinkets/platform/fabric/TrinketsFabric.java
+++ fabric/src/main/java/dev/emi/trinkets/platform/fabric/TrinketsFabric.java
@@ -1,9 +1,14 @@
 package dev.emi.trinkets.platform.fabric;
 
 import dev.emi.trinkets.TrinketsMain;
+import dev.emi.trinkets.TrinketsNetwork;
 import dev.emi.trinkets.data.EntitySlotLoader;
 import dev.emi.trinkets.data.SlotLoader;
+import dev.emi.trinkets.payload.BreakPayload;
+import dev.emi.trinkets.payload.SyncInventoryPayload;
+import dev.emi.trinkets.payload.SyncSlotsPayload;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.resource.ResourceType;
@@ -16,5 +21,9 @@ public class TrinketsFabric implements ModInitializer {
         ResourceManagerHelper resourceManagerHelper = ResourceManagerHelper.get(ResourceType.SERVER_DATA);
         resourceManagerHelper.registerReloadListener((IdentifiableResourceReloadListener) SlotLoader.INSTANCE);
         resourceManagerHelper.registerReloadListener((IdentifiableResourceReloadListener) EntitySlotLoader.SERVER);
+
+        PayloadTypeRegistry.playS2C().register(TrinketsNetwork.BREAK, BreakPayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(TrinketsNetwork.SYNC_INVENTORY, SyncInventoryPayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(TrinketsNetwork.SYNC_SLOTS, SyncSlotsPayload.CODEC);
     }
 }
